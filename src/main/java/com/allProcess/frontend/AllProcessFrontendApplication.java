badc25d82ecce2160706356d093585af7802ed5a
anthony2004lp <144872785+anthony2004lp@users.noreply.github.com>
2026-04-14 00:10:39 -0500

    Subida inicial con README

---
+++ src/main/java/com/allProcess/frontend/AllProcessFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.allProcess.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllProcessFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllProcessFrontendApplication.class, args);
+	}
+
+}
